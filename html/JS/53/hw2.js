daf376ca793d30fd9d1defafedce40b270fa0b8e
Reuvain Neufeld <ryn4310@gmail.com>
2020-07-02 21:34:26 -0400

    53

---
+++ html/JS/53/hw2.js
@@ -0,0 +1,18 @@
+ 
+'use strict';
+
+
+console.log('My name is', name);
+
+console.log('My email is', email);
+
+let age =prompt("What's your age ?");
+
+console.log('Your age is ',age);
+
+alert('Your age is ' + age);
+
+var i = confirm("Is it really your age?");
+
+
+
