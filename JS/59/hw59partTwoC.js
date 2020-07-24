604211e9f624f90d6f7d4c8128d1a5c93c77798f
Reuvain Neufeld <ryn4310@gmail.com>
2020-07-24 13:12:37 -0400

    updated Test/Quiz

--- JS/59/hw59partTwoC.js
+++ JS/59/hw59partTwoC.js
@@ -3,38 +3,28 @@
 for (let step = 0; step < 10; step++) {
 
     window.myApp.Count.Increment();
-} 
-// couldn't figure out why this for loop didn't work for the other ones.(I tried it.)
+}
+
 
 console.log('count Q2/A', window.myApp.Count.getCount());
 
 const cnt1 = window.app.count2.createCounter();
-cnt1.incrementCounter();
-cnt1.incrementCounter();
-cnt1.incrementCounter();
-cnt1.incrementCounter();
-cnt1.incrementCounter();
+
+
+for (let step = 0; step < 5; step++) {
+
+    cnt1.incrementCounter();
+}
 console.log('count1 Q2/b', cnt1.getCurrentCount());
 
 
 const cnt2 = window.app.count2.createCounter();
 
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
-cnt2.incrementCounter();
 
+for (let step = 0; step < 15; step++) {
+
+    cnt2.incrementCounter();
+}
 
 
 
@@ -42,3 +32,5 @@ console.log('count2 Q2/b', cnt2.getCurrentCount());
 
 
 console.log('number of "b" counters:', window.app.count2.getNumberOfCounters());
+
+
