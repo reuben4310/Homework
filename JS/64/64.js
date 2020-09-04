fc5e5646ace452377b729f18319dda288d9edb09
Reuvain Neufeld <ryn4310@gmail.com>
2020-09-03 22:35:33 -0400

    64

---
+++ JS/64/64.js
@@ -0,0 +1,36 @@
+/*global $ */
+
+$('body').append('<form>');
+
+$("form").append('Name: ', '<input type="text" placeholder="Name", name="name" id="name"/>');
+$('form').append(" Address: ", "<input type='text' placeholder='Address' id='rdescription' name='routedescription' class='address'/>");
+$('form').append("<br><br><input type='submit' disabled id='savebutton' value='Submit' />");
+$('form').append('<input type="checkbox" id="box" name="myCheckbox" />');
+$('form').append('<p>');
+
+
+$('#box').change(function () {
+    'use strict';
+
+    if ($("input[type=checkbox]").is(
+        ":checked")) {
+        $("#savebutton").prop('disabled', false);
+    } else {
+        $("#savebutton").prop('disabled', true);
+    }
+
+
+});
+
+$("#savebutton").click((event) => {
+    'use strict';
+    let sent = "Hi: ";
+    sent += $('#name').val();
+    sent += ". Your address is: ";
+    sent += $('#rdescription').val();
+    sent += ".";
+    $('p').text(sent);
+    $('form').css("background-color", "red");
+
+    event.preventDefault();
+});
