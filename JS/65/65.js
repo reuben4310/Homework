3b7ad8c14fad847829ba7f9cb9a8c508de99d617
Reuvain Neufeld <ryn4310@gmail.com>
2020-09-09 20:46:10 -0400

    updated 65

--- JS/65/65.js
+++ JS/65/65.js
@@ -4,25 +4,27 @@
     const page = $('#page');
     const load = $('#file');
     const theForm = $('#load');
-    $('#spin').hide();
-
+    const spin = $('#spin');
+    spin.hide();
 
 
     theForm.submit(event => {
 
-        $('#spin').show();
+        spin.show();
 
         event.preventDefault();
         setTimeout(() => {
             fetch(load.val())
                 .then(r => r.text())
                 .then((text) => page.text(text))
-                .catch(() => window.pcs.messageBox.show('ERORR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
-
+                .catch(() => window.pcs.messageBox.show('ERORR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', true))
+                .then(spin.hide());
             //theForm.trigger('reset');
             theForm[0].reset();
 
-        }, 1500);
+        }, 5000);
+
+
     });
 }());
 
