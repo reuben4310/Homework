d5dca985cd448b65cba7114addb56fd742b28dd1
reuben4310 <31813306+reuben4310@users.noreply.github.com>
2020-09-16 15:16:55 -0400

    Add files via upload

---
+++ 67.js
@@ -0,0 +1,35 @@
+/* global $ */
+(function () {
+    'use strict';
+
+    fetch('videos.json')
+        .then(r => {
+            if (!r.ok) {
+                throw new Error(`${r.status} ${r.statusText}`);
+            }
+            return r.json();
+        })
+        .then(videos => {
+
+            const videosList = $('#videos');
+            const video = $('#vid');
+            video.hide();
+            videos.forEach(vid => {
+                $(`<li>
+                <span>${vid.Title}</span>
+                <br>
+                    <img src= ${vid.Image} alt= ${vid.Title} 
+                </li>`)
+                    .appendTo(videosList).click(() => {
+                        video.show();
+                        video.attr('src', vid.URL);
+                        video[0].play();
+
+                    });
+
+            });
+
+        })
+        .catch(err => console.error(err));
+
+}());
