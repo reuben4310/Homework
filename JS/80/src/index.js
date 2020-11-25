5c3614a9fb4adbe218df8466adc361023d6582d8
Reuvain Neufeld <ryn4310@gmail.com>
2020-11-24 23:29:27 -0500

    DID NOT FINISH ALL THE EXERSIZES SINCE I GOOD NOT GET MY PROJECT TO WORK WEBPACK JUST MESSED EVERYTHING UP,& EVEN MR. WALDMAN WAS LOST

---
+++ JS/80/src/index.js
@@ -0,0 +1,68 @@
+
+
+'use strict';
+import '80.css';
+import $ from 'jquery';
+let dragging;
+let offset;
+let local = [];
+
+$(document).on('mousedown', '.parts', e => {
+    console.log('mousedown', e);
+    offset = { x: e.offsetX, y: e.offsetY };
+    // dragging = true;
+    dragging = $(e.target);
+});
+
+$(document/*.body*/).mousemove(e => {
+    if (dragging) {
+        e.preventDefault(); // occasional dragging wierdness in browser...
+
+        console.log('mousemove', e);
+
+        dragging.css({ top: e.pageY - offset.y, left: e.pageX - offset.x });
+    }
+}).mouseup(e => {
+    if (dragging) {
+        console.log('mouseup', e);
+        //dragging = false;
+        dragging = null;
+    }
+    $('.parts').each(function (i, parts) {
+        local.push({
+            id: $(parts).attr("id"),
+            top: $(parts).css('top'),
+            left: $(parts).css('left'),
+            zind: $(parts).css('z-index')
+        });
+    });
+    localStorage.local = JSON.stringify(local);
+});
+
+const body = $(document.body);
+const colorInput = $('#color');
+
+$('#add').click(() => {
+    //addMouseDownListener(
+    $('<div class="box"></div>').appendTo(body)
+        .css({ backgroundColor: colorInput.val() });
+    //);
+});
+let y = 0;
+$('.parts').each(function (i, obj) {
+    //test
+    $(obj).css('left', 60);
+    $(obj).css('top', y += 222);
+
+});
+
+if (localStorage.local) {
+    local = JSON.parse(localStorage.local);
+    local.forEach(rec => {
+        $(`#${rec.id}`).css('top', rec.top);
+        $(`#${rec.id}`).css('left', rec.left);
+        $(`#${rec.id}`).css('z-index', rec.zind);
+
+
+    });
+}
---
+++ JS/80/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        // commented out for hmr
+        // sayHello: './src/sayHello',npm run 
+        // another: './src/another'
+        watch: true,
+        
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            template: './src/80.html'
+        }),
+        new ESLintPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    //target: ['web', 'es5'], // webpack output wont work in IE11 default is es6
+    target: 'web', // bug in webpack dev server - cant have web and es5
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        // hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            /*{
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
+            }*/
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
